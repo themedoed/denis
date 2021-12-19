9edd575260d804ebd46321dd9f10d8f50b2e24b9
themedoed <93190084+themedoed@users.noreply.github.com>
2021-12-19 19:46:44 +0700

    Первый экран

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+});
+
+const anchors = document.querySelectorAll('.menu__list a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+  e.preventDefault()
+   
+  const blockID = anchor.getAttribute('href').substr(1)
+   
+  document.getElementById(blockID).scrollIntoView({
+  behavior: 'smooth',
+  block: 'start'
+  })
+  })
+}
